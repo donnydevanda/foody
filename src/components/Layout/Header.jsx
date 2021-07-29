03a6aca0f79f6e74b34658f0b4fc1cc5c718c07f
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-07-29 20:39:59 +0700

    logic + functionality

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,19 +1,21 @@
 import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
-import mealsImage from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Foody</h1>
-            <HeaderCartButton/>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="" />
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Foody</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="" />
+      </div>
     </Fragment>
+  );
 };
 
-export default Header
+export default Header;
